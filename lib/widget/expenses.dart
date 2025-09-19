84e8a9919e95f3801ae584a722adf6ca4c5399b6
Yousif Khalid <yousifkhalid544@gmail.com>
2025-09-18 17:02:09 -0700

    adding title text field

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -27,7 +28,7 @@ class _ExpensesState extends State<Expenses> {
   ];
   // model sheet
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(context: context, builder: (ctx) => Text('data'));
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
   }
 
   @override
