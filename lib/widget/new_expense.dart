31fa8b7156bce3495eb33abcafa433947068e595
Yousif Khalid <yousifkhalid544@gmail.com>
2025-09-23 16:41:31 -0700

    adding amount textField & cancel button

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -8,6 +8,16 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final titleControler = TextEditingController();
+  final amountControler = TextEditingController();
+
+  @override
+  void dispose() {
+    titleControler.dispose();
+    amountControler.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,9 +25,40 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           // add new expense
+          // expense title
           TextField(
+            controller: titleControler,
             maxLength: 50,
             decoration: InputDecoration(label: Text('Title')),
+          ),
+          // expense amount
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: amountControler,
+            decoration: InputDecoration(
+              prefixText: '\$ ',
+              label: Text('amount'),
+            ),
+          ),
+          Row(
+            children: [
+              Spacer(),
+              // Cancel Expense Button
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
+              // Save Expense button
+              ElevatedButton(
+                onPressed: () {
+                  print(titleControler.text);
+                  print(amountControler.text);
+                },
+                child: Text('Save Expense'),
+              ),
+            ],
           ),
         ],
       ),
